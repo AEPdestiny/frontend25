967f83e4663cd2d4fd03e11d19574f4edb3b6262
Ibrahim Danisman <121925671+AEPdestiny@users.noreply.github.com>
2025-07-15 06:19:41 +0200

    edit

--- Kleidung/src/services/kleidungService.ts
+++ Kleidung/src/services/kleidungService.ts
@@ -2,16 +2,16 @@
 import axios from 'axios'
 
 export const fetchKleidung = async () => {
-  const response = await axios.get('/api/kleidung/user')
+  const response = await axios.get('/api/kleidung/user', { withCredentials: true })
   return response.data
 }
 
 export const createKleidung = async (data: any) => {
-  const response = await axios.post('/api/kleidung', data)
+  const response = await axios.post('/api/kleidung', data, { withCredentials: true })
   return response.data
 }
 
 export const updateKleidung = async (id: number, data: any) => {
-  const response = await axios.put(`/api/kleidung/${id}`, data)
+  const response = await axios.put(`/api/kleidung/${id}`, data, { withCredentials: true })
   return response.data
 }
